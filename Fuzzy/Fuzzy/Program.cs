66692dcb649378f02afe4210b67adb80d9afde86
Mattew Robison <anxel9302@gmail.com>
2017-02-12 23:10:48 -0500

    creation of fuzzy project
    playing with linear interpolation to acheive degree of memeber ship for
    a fuzzy set.

---
+++ Fuzzy/Fuzzy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy
+{
+    public class point
+    {
+        public double x,y;
+
+        public point(double x, double y){
+            this.x = x;
+            this.y = y;
+            }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is how we get the value of a points degree of membership
+            point one = new point(5.0, 1.0);
+            point two = new point(10.0, 0);
+            double value = liner_interpolation(one, two, 9.84);
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+        }
+
+        static public double liner_interpolation(point a, point b, double x)
+        {
+            double slope = ((b.y - a.y) / (b.x - a.x));
+            Console.WriteLine("Slope is :" + slope);
+            double second_term = x - a.x;
+            Console.WriteLine("Second Term is: " + second_term);
+            double tmp = a.y + second_term * slope;            
+            return tmp;
+        }
+    }
+}
